19f431c1a2f8731141ad61a28424f337a3d7eb4b
adhamabzd <adham_abzd@aucegypt.edu>
2025-05-16 22:44:41 +0300

    created an new level design
    edited level2.cpp and created a tentative design

--- src/levels/level2.cpp
+++ src/levels/level2.cpp
@@ -1,4 +1,7 @@
 #include "level2.h"
+#include "environment/platform.h"
+
+
 
 Level2::Level2(QGraphicsView* view, Kimo* kimo, QGraphicsTextItem* healthText, QGraphicsTextItem* levelText, QObject *parent)
     : BaseLevel(view, kimo, healthText, levelText, parent) {}
@@ -9,4 +12,93 @@ void Level2::setEnemies() {
 
 void Level2::setEnvironment() {
     // Add the platforms, spikes, background, etc. for Level 2
+
+    // Background
+    view->setBackgroundBrush(QPixmap(":/images/levels/background.png"));
+
+    // Ground platform
+    StaticPlatform *ground1 = new StaticPlatform(300, 50, 0, 550);
+    addItem(ground1);
+
+    StaticPlatform *ground2 = new StaticPlatform(100, 50, 400, 550);
+    addItem(ground2);
+
+    StaticPlatform *ground3 = new StaticPlatform(150, 50, 800, 550);
+    addItem(ground3);
+
+    StaticPlatform *ground4 = new StaticPlatform(100, 50, 1050, 550);
+    addItem(ground4);
+
+    StaticPlatform *ground5 = new StaticPlatform(300, 50, 1250, 550);
+    addItem(ground5);
+
+    StaticPlatform *step1 = new StaticPlatform(100, 50, 1250, 500);
+    addItem(step1);
+
+    StaticPlatform *step2 = new StaticPlatform(100, 100, 1350, 450);
+    addItem(step2);
+
+    StaticPlatform *step3 = new StaticPlatform(100, 150, 1450, 400);
+    addItem(step3);
+
+    StaticPlatform *ground6 = new StaticPlatform(350, 50, 1650, 550);
+    addItem(ground6);
+
+    // Spiky platforms
+    SpikyPlatform *spiky1 = new SpikyPlatform(100, 20, 300, 580);
+    addItem(spiky1);
+
+    SpikyPlatform *spiky2 = new SpikyPlatform(100, 20, 500, 580);
+    addItem(spiky2);
+
+    SpikyPlatform *spiky3 = new SpikyPlatform(100, 20, 600, 580);
+    addItem(spiky3);
+
+    SpikyPlatform *spiky4 = new SpikyPlatform(100, 20, 700, 580);
+    addItem(spiky4);
+
+    SpikyPlatform *spiky5 = new SpikyPlatform(100, 20, 950, 580);
+    addItem(spiky5);
+
+    SpikyPlatform *spiky6 = new SpikyPlatform(100, 20, 700, 580);
+    addItem(spiky6);
+
+    SpikyPlatform *spiky7 = new SpikyPlatform(100, 20, 1550, 580);
+    addItem(spiky7);
+
+    // Floating static plaforms
+    StaticPlatform *platform1 = new StaticPlatform(150, 20, 100, 400);
+    addItem(platform1);
+
+    StaticPlatform *platform2 = new StaticPlatform(150, 20, 300, 300);
+    addItem(platform2);
+
+    StaticPlatform *platform3 = new StaticPlatform(150, 20, 800, 300);
+    addItem(platform3);
+
+    // Moving platform (moves horizontally)
+    MovingPlatform *moving1 = new MovingPlatform(150, 20, 500, 200, 200, 2);
+    addItem(moving1);
+
+    MovingPlatform *moving2 = new MovingPlatform(150, 20, 1050, 250, 250, 3);
+    addItem(moving2);
+
+    // Adding temporary 'Clear Condition' object
+    QGraphicsRectItem* goal = new QGraphicsRectItem(0,0,64,64);
+    goal->setBrush(Qt::red);
+    goal->setPos(1800, 450);
+    addItem(goal);
+    kimo->setGoal(goal);
+
+    view->show(); // Show the view
+
+    // Timer for game updates (platforms, HUD)
+    QTimer *gameUpdateTimer = new QTimer();
+    QObject::connect(gameUpdateTimer, &QTimer::timeout, [=]() {
+        // Update moving platforms
+        moving1->update();
+        moving2->update();
+    });
+
+    gameUpdateTimer->start(16); // ~60 FPS
 }
