ad05c87ed4a8e5675f3437be6a2473b8e60063ee
adhamabzd <adham_abzd@aucegypt.edu>
2025-05-18 19:45:59 +0300

    added level 4 design

--- src/levels/level4.cpp
+++ src/levels/level4.cpp
@@ -17,20 +17,98 @@ void Level4::setEnvironment() {
     StaticPlatform *ground1 = new StaticPlatform(300, 50, 0, 550);
     addItem(ground1);
 
+<<<<<<< Updated upstream
+    StaticPlatform *ground2 = new StaticPlatform(104, 50, 730, 550);
+    addItem(ground2);
+
+    StaticPlatform *ground3 = new StaticPlatform(264, 50, 906, 550);
+    addItem(ground3);
+
+    StaticPlatform *ground4 = new StaticPlatform(100, 300, 1170, 300);
+    addItem(ground4);
+
+    StaticPlatform *ground5 = new StaticPlatform(100, 50, 1270, 550);
+    addItem(ground5);
+
+    StaticPlatform *ground6 = new StaticPlatform(50, 50, 1470, 550);
+    addItem(ground6);
+
+    StaticPlatform *ground7 = new StaticPlatform(380, 50, 1620, 550);
+    addItem(ground7);
+
+    StaticPlatform *step1 = new StaticPlatform(104, 125, 730, 475);
+    addItem(step1);
+
+    StaticPlatform *step2 = new StaticPlatform(104, 125, 906, 475);
+=======
+    StaticPlatform *ground2 = new StaticPlatform(120, 50, 730, 550);
+    addItem(ground2);
+
+    StaticPlatform *ground3 = new StaticPlatform(280, 50, 890, 550);
+    addItem(ground3);
+
+    StaticPlatform *ground4 = new StaticPlatform(150, 300, 1170, 300);
+    addItem(ground4);
+
+    StaticPlatform *step1 = new StaticPlatform(120, 125, 730, 475);
+    addItem(step1);
+
+    StaticPlatform *step2 = new StaticPlatform(120, 125, 890, 475);
+>>>>>>> Stashed changes
+    addItem(step2);
+
     // Spiky platforms
     SpikyPlatform *spiky1 = new SpikyPlatform(100, 20, 300, 580);
     addItem(spiky1);
 
+    SpikyPlatform *spiky2 = new SpikyPlatform(100, 20, 630, 580);
+    addItem(spiky2);
+
+<<<<<<< Updated upstream
+    SpikyPlatform *spiky3 = new SpikyPlatform(100, 20, 1370, 580);
+    addItem(spiky3);
+
+    SpikyPlatform *spiky4 = new SpikyPlatform(100, 20, 1520, 580);
+    addItem(spiky4);
+
+=======
+>>>>>>> Stashed changes
     // Floating static plaforms
-    StaticPlatform *platform1 = new StaticPlatform(150, 20, 100, 400);
+    StaticPlatform *platform1 = new StaticPlatform(100, 20, 300, 400);
     addItem(platform1);
 
+    StaticPlatform *platform2 = new StaticPlatform(100, 20, 630, 400);
+    addItem(platform2);
+
     // Moving platform (moves horizontally)
-    MovingPlatform *moving1 = new MovingPlatform(150, 20, 500, 200, 200, 2);
+    MovingPlatform *moving1 = new MovingPlatform(100, 20, 380, 300, 150, 2);
     addItem(moving1);
 
     // Moving platform (moves vertically)
-    VerticallyMovingPlatform *movingup = new VerticallyMovingPlatform(150, 20, 800, 400, 200, 2);
+    VerticallyMovingPlatform *movingup = new VerticallyMovingPlatform(100, 20, 1030, 300, 200, 2);
     addItem(movingup);
+
+<<<<<<< Updated upstream
+    // Adding temporary 'Clear Condition' object
+    QGraphicsRectItem* goal = new QGraphicsRectItem(0,0,64,64);
+    goal->setBrush(Qt::red);
+    goal->setPos(1800, 450);
+    addItem(goal);
+    kimo->setGoal(goal);
+
+    view->show(); // Show the view
+
+=======
+>>>>>>> Stashed changes
+    // Timer for game updates (platforms, HUD)
+    gameUpdateTimer = new QTimer(this);
+    connect(gameUpdateTimer, &QTimer::timeout, this, [=]() {
+        if (!moving1 || !movingup) return;
+        // Update moving platforms
+        moving1->update();
+        movingup->update();
+    });
+
+    gameUpdateTimer->start(16); // ~60 FPS
 
 }
