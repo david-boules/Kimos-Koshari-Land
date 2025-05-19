dcd6c2fc2e97b9e59c55b5da8be83607d60ff261
adhamabzd <adham_abzd@aucegypt.edu>
2025-05-19 22:40:10 +0300

    reverted level 4

--- src/levels/level4.cpp
+++ src/levels/level4.cpp
@@ -13,24 +13,32 @@ void Level4::setEnvironment() {
 
     view->setBackgroundBrush(QPixmap(":/images/levels/L4background.png"));
 
-    // Ground platforms
+    // Ground platform
     StaticPlatform *ground1 = new StaticPlatform(300, 50, 0, 550);
     addItem(ground1);
 
-    StaticPlatform *ground2 = new StaticPlatform(120, 50, 730, 550);
+    StaticPlatform *ground2 = new StaticPlatform(104, 50, 730, 550);
     addItem(ground2);
 
-    StaticPlatform *ground3 = new StaticPlatform(280, 50, 890, 550);
+    StaticPlatform *ground3 = new StaticPlatform(264, 50, 906, 550);
     addItem(ground3);
 
-    StaticPlatform *ground4 = new StaticPlatform(150, 300, 1170, 300);
+    StaticPlatform *ground4 = new StaticPlatform(100, 300, 1170, 300);
     addItem(ground4);
 
-    // Step platforms
-    StaticPlatform *step1 = new StaticPlatform(120, 125, 730, 475);
+    StaticPlatform *ground5 = new StaticPlatform(100, 50, 1270, 550);
+    addItem(ground5);
+
+    StaticPlatform *ground6 = new StaticPlatform(50, 50, 1470, 550);
+    addItem(ground6);
+
+    StaticPlatform *ground7 = new StaticPlatform(380, 50, 1620, 550);
+    addItem(ground7);
+
+    StaticPlatform *step1 = new StaticPlatform(104, 125, 730, 475);
     addItem(step1);
 
-    StaticPlatform *step2 = new StaticPlatform(120, 125, 890, 475);
+    StaticPlatform *step2 = new StaticPlatform(104, 125, 906, 475);
     addItem(step2);
 
     // Spiky platforms
@@ -40,20 +48,36 @@ void Level4::setEnvironment() {
     SpikyPlatform *spiky2 = new SpikyPlatform(100, 20, 630, 580);
     addItem(spiky2);
 
-    // Floating static platforms
+    SpikyPlatform *spiky3 = new SpikyPlatform(100, 20, 1370, 580);
+    addItem(spiky3);
+
+    SpikyPlatform *spiky4 = new SpikyPlatform(100, 20, 1520, 580);
+    addItem(spiky4);
+
+
+    // Floating static plaforms
     StaticPlatform *platform1 = new StaticPlatform(100, 20, 300, 400);
     addItem(platform1);
 
     StaticPlatform *platform2 = new StaticPlatform(100, 20, 630, 400);
     addItem(platform2);
 
-    // Moving platform (horizontal)
+    // Moving platform (moves horizontally)
     MovingPlatform *moving1 = new MovingPlatform(100, 20, 380, 300, 150, 2);
     addItem(moving1);
 
-    // Moving platform (vertical)
+    // Moving platform (moves vertically)
     VerticallyMovingPlatform *movingup = new VerticallyMovingPlatform(100, 20, 1030, 300, 200, 2);
     addItem(movingup);
+
+    // Adding temporary 'Clear Condition' object
+    QGraphicsRectItem* goal = new QGraphicsRectItem(0,0,64,64);
+    goal->setBrush(Qt::red);
+    goal->setPos(1800, 450);
+    addItem(goal);
+    kimo->setGoal(goal);
+
+    view->show(); // Show the view
 
     // Timer for game updates (platforms, HUD)
     gameUpdateTimer = new QTimer(this);
@@ -65,5 +89,5 @@ void Level4::setEnvironment() {
     });
 
     gameUpdateTimer->start(16); // ~60 FPS
-}
 
+}
